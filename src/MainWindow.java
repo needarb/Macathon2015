712880c390ba87b1a2e01cc8dc09d5f8f55afcb7
Valenti <cvalenti@macalester.edu>
2015-02-20 21:28:53 -0600

    connor changes

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -15,7 +15,7 @@ public class MainWindow extends JFrame
         super();
         setTitle("Ned was here");
         add(new JButton("I AM A BUTTON"));
-        add(new JLabel("I like turtles!"));
+        add(new JLabel("I like a;osldkfja;lsdkfj!"));
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         pack();
         setVisible(true);
