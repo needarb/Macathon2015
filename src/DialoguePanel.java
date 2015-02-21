a2800db7a2ada4d1dd03f6614d3f3b9ebd9ef66e
Benjamin Mathers <bmathers@macalester.edu>
2015-02-21 06:02:06 -0600

    Dialogue Panel completed in NetBeans. Updates probably needed for IntelliJ

--- src/DialoguePanel.java
+++ src/DialoguePanel.java
@@ -4,6 +4,11 @@
  * and open the template in the editor.
  */
 
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+
 /**
  *
  * @author Benjamin Mathers
@@ -13,6 +18,8 @@ public class DialoguePanel extends javax.swing.JPanel {
     /**
      * Creates new form DialoguePanel2
      */
+
+
     public DialoguePanel() {
         initComponents();
     }
@@ -42,14 +49,23 @@ public class DialoguePanel extends javax.swing.JPanel {
         jPanel3 = new javax.swing.JPanel();
         changeIDStateButton = new javax.swing.JButton();
         endDialogueButton = new javax.swing.JButton();
+        leftSidePanel = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        rightSidePanel = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
 
-        setBackground(new java.awt.Color(50, 216, 204));
+        setBackground(new java.awt.Color(255, 255, 255));
         setPreferredSize(new java.awt.Dimension(1012, 751));
 
-        jLabel2.setFont(new java.awt.Font("Vani", 1, 36)); // NOI18N
+        jPanel1.setBackground(new java.awt.Color(150, 0, 118));
+
+        jLabel2.setFont(new java.awt.Font("Vani", 1, 48)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("Bear or Orangutan");
         jPanel1.add(jLabel2);
 
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel2.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(255, 218, 247)));
         jPanel2.setRequestFocusEnabled(false);
         jPanel2.setLayout(new java.awt.GridLayout(2, 3, 0, 15));
 
@@ -68,8 +84,11 @@ public class DialoguePanel extends javax.swing.JPanel {
         jPanel2.add(jScrollPane1);
 
         companionLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        companionLabel.setText("Anonymous Magpie");
+        companionLabel.setText("Speaking With Anonymous Magpie");
         jPanel2.add(companionLabel);
+
+        messagePanel.setBackground(new java.awt.Color(255, 255, 255));
+        messagePanel.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(255, 218, 247)));
 
         messageArea.setEditable(false);
         messageArea.setColumns(20);
@@ -77,6 +96,7 @@ public class DialoguePanel extends javax.swing.JPanel {
         messageArea.setRows(5);
         messageArea.setWrapStyleWord(true);
         messageArea.setAutoscrolls(false);
+        messageArea.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(255, 218, 247)));
         messageArea.setFocusable(false);
         messageArea.setRequestFocusEnabled(false);
         jScrollPane2.setViewportView(messageArea);
@@ -87,22 +107,25 @@ public class DialoguePanel extends javax.swing.JPanel {
                 messagePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(messagePanelLayout.createSequentialGroup()
                                 .addContainerGap()
-                                .addComponent(jScrollPane2)
+                                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 531, Short.MAX_VALUE)
                                 .addContainerGap())
         );
         messagePanelLayout.setVerticalGroup(
                 messagePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(messagePanelLayout.createSequentialGroup()
                                 .addContainerGap()
-                                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 407, Short.MAX_VALUE)
+                                .addComponent(jScrollPane2)
                                 .addContainerGap())
         );
+
+        responePanel.setBackground(new java.awt.Color(255, 255, 255));
 
         responseArea.setColumns(20);
         responseArea.setLineWrap(true);
         responseArea.setRows(5);
         responseArea.setWrapStyleWord(true);
         responseArea.setAutoscrolls(false);
+        responseArea.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(255, 218, 247)));
         responseArea.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
                 responseAreaKeyPressed(evt);
@@ -141,9 +164,21 @@ public class DialoguePanel extends javax.swing.JPanel {
                                 .addContainerGap())
         );
 
+        jPanel3.setBackground(new java.awt.Color(255, 255, 255));
+
         changeIDStateButton.setText("Reveal ID");
+        changeIDStateButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                changeIDStateButtonActionPerformed(evt);
+            }
+        });
 
         endDialogueButton.setText("End Dialogue");
+        endDialogueButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                endDialogueButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
@@ -154,7 +189,7 @@ public class DialoguePanel extends javax.swing.JPanel {
                                 .addComponent(changeIDStateButton)
                                 .addGap(18, 18, 18)
                                 .addComponent(endDialogueButton)
-                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addContainerGap(349, Short.MAX_VALUE))
         );
         jPanel3Layout.setVerticalGroup(
                 jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -166,32 +201,90 @@ public class DialoguePanel extends javax.swing.JPanel {
                                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
+        leftSidePanel.setBackground(new java.awt.Color(255, 255, 255));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/res/images/plaid.jpg"))); // NOI18N
+        jLabel1.setText("jLabel1");
+        jLabel1.setMaximumSize(new java.awt.Dimension(122, 19));
+        jLabel1.setMinimumSize(new java.awt.Dimension(122, 19));
+        jLabel1.setPreferredSize(new java.awt.Dimension(122, 19));
+
+        javax.swing.GroupLayout leftSidePanelLayout = new javax.swing.GroupLayout(leftSidePanel);
+        leftSidePanel.setLayout(leftSidePanelLayout);
+        leftSidePanelLayout.setHorizontalGroup(
+                leftSidePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(leftSidePanelLayout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 179, Short.MAX_VALUE)
+                                .addContainerGap())
+        );
+        leftSidePanelLayout.setVerticalGroup(
+                leftSidePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(leftSidePanelLayout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addContainerGap())
+        );
+
+        rightSidePanel.setBackground(new java.awt.Color(255, 255, 255));
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/res/images/plaid.jpg"))); // NOI18N
+        jLabel3.setText("jLabel1");
+        jLabel3.setMaximumSize(new java.awt.Dimension(122, 19));
+        jLabel3.setMinimumSize(new java.awt.Dimension(122, 19));
+        jLabel3.setPreferredSize(new java.awt.Dimension(122, 19));
+
+        javax.swing.GroupLayout rightSidePanelLayout = new javax.swing.GroupLayout(rightSidePanel);
+        rightSidePanel.setLayout(rightSidePanelLayout);
+        rightSidePanelLayout.setHorizontalGroup(
+                rightSidePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(rightSidePanelLayout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 196, Short.MAX_VALUE)
+                                .addContainerGap())
+        );
+        rightSidePanelLayout.setVerticalGroup(
+                rightSidePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(rightSidePanelLayout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addContainerGap())
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(layout.createSequentialGroup()
-                                .addGap(231, 231, 231)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                        .addComponent(responePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(messagePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(jPanel3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                                .addContainerGap(230, Short.MAX_VALUE))
+                                .addContainerGap()
+                                .addComponent(leftSidePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(responePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(messagePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 551, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(rightSidePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap())
         );
         layout.setVerticalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
                                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(messagePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addGap(18, 18, 18)
-                                .addComponent(responePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                                .addComponent(messagePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                .addGap(18, 18, 18)
+                                                .addComponent(responePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addGap(18, 18, 18)
+                                                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(leftSidePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(rightSidePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                                 .addContainerGap())
         );
     }// </editor-fold>                        
@@ -208,14 +301,60 @@ public class DialoguePanel extends javax.swing.JPanel {
         }
     }
 
+    private void changeIDStateButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        changeIDStateButton.setText("ID Revealed");
+        changeIDStateButton.setEnabled(false);
+        String existingText = messageArea.getText();
+        messageArea.setText(existingText + System.lineSeparator() + "Reveal Request Sent" + System.lineSeparator());
+        Timer timer = new Timer( 1000, new ActionListener(){
+            @Override
+            public void actionPerformed( ActionEvent e ){
+                String existingText = messageArea.getText();
+                String bResponse = "Anonymous Magpie has accepted your request!" + System.lineSeparator() + "Anonymous Narwhal is Ben Mathers" + System.lineSeparator() + "Anonymous Magpie is Matthias Broderson";
+                messageArea.setText(existingText + bResponse + System.lineSeparator());
+            }
+        });
+        timer.setRepeats(false);
+        timer.start();
+    }
+
+    private void endDialogueButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        String existingText = messageArea.getText();
+        messageArea.setText(System.lineSeparator() + "Anonymous Narwhal has opted to end the dialogue");
+        Timer timer = new Timer( 1000, new ActionListener(){
+            @Override
+            public void actionPerformed( ActionEvent e ){
+                System.exit(0);
+            }
+        });
+        timer.setRepeats(false);
+        timer.start();
+    }
+
     private void updateTextArea(){
         String text = responseArea.getText();
         String existingText = messageArea.getText();
         messageArea.setText(existingText + "Anonymous Narwhal: " + text + System.lineSeparator());
-        existingText = messageArea.getText();
-        String bResponse = "Anonymous Magpie: Orangutan, for sure";
-        messageArea.setText(existingText + bResponse + System.lineSeparator());
+        Timer timer = new Timer( 1000, new ActionListener(){
+            @Override
+            public void actionPerformed( ActionEvent e ){
+                String existingText = messageArea.getText();
+                String bResponse = "Anonymous Magpie: But Orangutans are so smart and cuddly! Why do you like Bears?";
+                messageArea.setText(existingText + bResponse + System.lineSeparator());
+            }
+        });
+        timer.setRepeats( false );
+        timer.start();
+
         responseArea.setText("");
+    }
+
+    private void delay(){
+        Timer timer = new Timer(1000, new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){}
+        });
+        timer.setInitialDelay(1000);
     }
 
     // Variables declaration - do not modify                     
@@ -223,17 +362,21 @@ public class DialoguePanel extends javax.swing.JPanel {
     private javax.swing.JLabel companionLabel;
     private javax.swing.JTextArea descriptionArea;
     private javax.swing.JButton endDialogueButton;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JPanel leftSidePanel;
     private javax.swing.JTextArea messageArea;
     private javax.swing.JPanel messagePanel;
     private javax.swing.JPanel responePanel;
     private javax.swing.JTextArea responseArea;
+    private javax.swing.JPanel rightSidePanel;
     private javax.swing.JButton sendButton;
     // End of variables declaration                   
 }
